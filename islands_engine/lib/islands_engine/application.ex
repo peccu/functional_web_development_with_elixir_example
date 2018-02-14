10978b3e99cab97d0eb6e1536b950c829a5f1521
peccu <peccul@gmail.com>
2018-02-14 13:27:28 +0900

    update to ch.5

--- islands_engine/lib/islands_engine/application.ex
+++ islands_engine/lib/islands_engine/application.ex
@@ -1,3 +1,11 @@
+#---
+# Excerpted from "Functional Web Development with Elixir, OTP, and Phoenix",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/lhelph for more book information.
+#---
 defmodule IslandsEngine.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
@@ -10,8 +18,12 @@ defmodule IslandsEngine.Application do
     children = [
       # Starts a worker by calling: IslandsEngine.Worker.start_link(arg)
       # {IslandsEngine.Worker, arg},
-      {Registry, keys: :unique, name: Registry.Game}
+      {Registry, keys: :unique, name: Registry.Game},
+      IslandsEngine.GameSupervisor
     ]
+
+    # store data
+    :ets.new(:game_state, [:public, :named_table])
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
